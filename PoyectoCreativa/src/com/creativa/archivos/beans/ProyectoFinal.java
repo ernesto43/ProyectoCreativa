0f7208f104beaed0aea68f2792cd65543a4a5147
jorge.flores <jorge.flores@W8CAPA06.creativa.com>
2013-10-16 10:50:43 -0600

    -bean proyecto final

---
+++ PoyectoCreativa/src/com/creativa/archivos/beans/ProyectoFinal.java
@@ -0,0 +1,14 @@
+package com.creativa.archivos.beans;
+
+public class ProyectoFinal {
+	String nombreProducto;
+	String precioProducto;
+	double cantidadProducto;
+	
+	public ProyectoFinal(){}
+	
+	public ProyectoFinal(String nombreProducto, String precioProducto, double cantidadProducto){
+		
+	}
+
+}
