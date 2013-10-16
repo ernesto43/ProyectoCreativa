469846c037aab1b99c0eb594ffbd49d468bf8eca
jorge.flores <jorge.flores@W8CAPA06.creativa.com>
2013-10-16 10:54:27 -0600

    -bean terminado

--- PoyectoCreativa/src/com/creativa/archivos/beans/ProyectoFinal.java
+++ PoyectoCreativa/src/com/creativa/archivos/beans/ProyectoFinal.java
@@ -8,7 +8,36 @@ public class ProyectoFinal {
 	public ProyectoFinal(){}
 	
 	public ProyectoFinal(String nombreProducto, String precioProducto, double cantidadProducto){
-		
+		super();
+		this.nombreProducto = nombreProducto;
+		this.precioProducto = precioProducto;
+		this.cantidadProducto = cantidadProducto;
 	}
+
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+
+	public void setNombreProducto(String nombreProducto) {
+		this.nombreProducto = nombreProducto;
+	}
+
+	public String getPrecioProducto() {
+		return precioProducto;
+	}
+
+	public void setPrecioProducto(String precioProducto) {
+		this.precioProducto = precioProducto;
+	}
+
+	public double getCantidadProducto() {
+		return cantidadProducto;
+	}
+
+	public void setCantidadProducto(double cantidadProducto) {
+		this.cantidadProducto = cantidadProducto;
+	}
+	
+	
 
 }
